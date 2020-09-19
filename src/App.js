2fadc5897f8d4db1b1d1e3190fbb15690d567649
Florhaidee <florhaideedev@gmail.com>
2020-09-18 23:43:18 -0600

    contact component can conditionally render

--- src/App.js
+++ src/App.js
@@ -14,21 +14,27 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
-
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
+          {!contactSelected ? (
+          <>
           <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
+          </>
+          ) : (
+          <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
   return (
@@ -18,17 +20,20 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => {
+              //setCurrentCategory(category)
+              setContactSelected(true);
+            }}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                currentCategory.name === category.name && !contactSelected && `navActive`
                 }`}
               key={category.name}
             >
